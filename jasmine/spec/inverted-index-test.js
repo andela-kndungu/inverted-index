c6a7a6a62ecd1c6c958a89453a4981b7f71d206c
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-03-15 23:10:12 +0300

    Add populate index test suite

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,1 +1,93 @@
-alert('Spec file works');
+describe('Read book data', function() {
+
+  // Object that contains all required functions
+  var invertedIndex;
+
+  // Make instance to access functions
+  beforeAll(function(done) {
+
+    invertedIndex = new InvertedIndex();
+
+    // Store returned books array as object property
+    invertedIndex.loadJSON('../books.json').done(function(data) {
+
+      invertedIndex.booksArray = data;
+    });
+  });
+
+  // Test function that reads and parses JSON file
+  it('correctly reads and parses json file', function() {
+
+    expect(invertedIndex.booksArray).toBeDefined();
+    expect(invertedIndex.booksArray).toEqual(jasmine.any(Object));
+    exopect(invertedIndex.booksArray).not.toEqual({});
+  });
+});
+
+describe('Populate Index', function() {
+
+  // Object that contains all required functions
+  var invertedIndex;
+
+  // Make instance to access functions
+  beforeAll(function(done) {
+
+    invertedIndex = new InvertedIndex();
+
+    // Store returned books array as object property
+    invertedIndex.loadJSON('../books.json').done(function(data) {
+
+      invertedIndex.booksArray = data;
+    });
+
+    // Concatenate objects' strings for easier lookup
+    function concat(obj) {
+
+      var keys = Object.keys(obj);
+      var concatString = '';
+
+      keys.forEach(function(key) {
+
+        concatString += obj.key;
+      });
+
+      return concatString;
+    }
+
+    // Convert booksArray into array of constituting strings
+    for(var i = 0; i < (invertedIndex.booksArray).length; i++) {
+
+      invertedIndex.booksArray[i] = concat(invertedIndex.booksArray[i]);
+    }
+  });
+
+  // Test whetehr index is created as soon as JSON loads
+  it('correctly creates index from loaded JSON', function() {
+
+    expect(invertedIndex.index).toBeDefined();
+    expect(invertedIndex.index).toEqual(jasmine.any(Object));
+    exopect(invertedIndex.index).not.toEqual({});
+  });
+
+  // Test whetehr index maps the string keys to the correct objects
+  it('correctly maps keys to objects', function() {
+
+    // String properties of the index
+    var tokens = Object.keys(invertedIndex.index);
+
+    // For each property
+    tokens.forEach(function(property) {
+
+      // Get array of objects associated with it
+      var objsArray = invertedIndex.index.property;
+
+      // For each concatenated string in the array
+      objsArray.forEach(function(conStr) {
+
+        // Check whether string property is in it
+        expect(conStr.indexOf(token)).toBeGreaterThan(-1);
+
+      });
+    });
+  });
+});
--- jasmine/src/inverted-index.js
+++
@@ -1,1 +0,0 @@
-alert('src file works');
